686fa7f27081aa905f9c37d4b940fb367a270f2e
Arthur Santos <arthurluiz.developer@gmail.com>
2025-03-12 14:11:57 +0000

    Programa base.

---
+++ BetterDecorador/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BetterDecorador
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Decor("Good Morning!", '*'));
+            Console.WriteLine(Decor("Good Evening!", '.'));
+        }
+
+        private static string Decor(string s, char dec)
+        {
+            return $"{dec}{dec}{dec} {s} {dec}{dec}{dec}";
+        }
+    }
+}
